c46f666ff12a1efe7fa14506efef2dd4ddeb3076
Andrés Liu <andresdavidliu@hotmail.com>
2017-04-19 12:16:49 -0500

    Avances en búsqueda de palabras relacionadas
    Implementación de búsqueda de palabras relacionadas al término de
    búsqueda dentro del diccionario.

--- diccionario/js/diccionario.js
+++ diccionario/js/diccionario.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
 
     var $input = $(".diccionario__search-input");
+    var $resultados = $(".diccionario__resultados-lista");
+    var $relacionadas = $(".diccionario__relacionadas-lista");
 
     $input.change(function (e) {
         var palabra = $input.val();
         buscarPalabra(palabra);
     });
+
+    $input.focusin(function (e) {
+        limpiarPalabras();
+    });
+
+    function limpiarPalabras() {
+        $resultados.empty();
+        $relacionadas.empty();
+    }
 
     var $caracter = $(".palabra__caracter");
 
@@ -14,26 +25,45 @@ $(document).ready(function () {
         $.post("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170321T235648Z.3193900a2fbf206f.5f78e83dd4de5da554f974b1976faff175639c28&text=" + palabra + "&lang=es-zh", function (res) {
             var caracter = res.text[0];
             // Se crea la estructura para el template de la palabra
-            var $palabra = crearTemplate();
+            var $palabra = crearTemplate($resultados);
             $palabra.find(".palabra__caracter").text(caracter);
             // Se traduce a pinyin
             var pinyin = cjst.chineseToPinyin(caracter).join(' ');
-            console.log(pinyin);
             $palabra.find(".palabra__info-item-value--pronunciacion").text(pinyin);
             // Permite la pronunciación
             $palabra.find(".palabra__info-item-play").click(function () {
                 pronunciar(caracter);
             });
-            // Muestra el significado
-            definirPalabra(palabra, $palabra.find(".palabra__info-item-value--significado"));
+            // Se realiza la traducción
+            $.post("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170321T235648Z.3193900a2fbf206f.5f78e83dd4de5da554f974b1976faff175639c28&text=" + palabra + "&lang=es-en", function (res) {
+                var traduccionIngles = res.text[0].toLowerCase();
+                // Muestra el significado
+                definirPalabra(traduccionIngles, $palabra.find(".palabra__info-item-value--significado"));
+                // Muestra las palabras relacionadas
+                var relacionadas = buscarRelacionadas(traduccionIngles, caracter, pinyin);
+                console.log(relacionadas);
+                for (var i = 0; i < relacionadas.length; i++) {
+                    var $palabraRelacionada = crearTemplate($relacionadas);
+                    definirPalabra(relacionadas[i], $palabraRelacionada.find(".palabra__info-item-value--significado"));
+                }
+            });
         });
     }
 
-    var $resultados = $(".diccionario__resultados");
+    function buscarRelacionadas(traduccionIngles) {
+        var relacionadas = [];
+        $.get("https://api.datamuse.com/words?ml=" + traduccionIngles, function (res) {
+            for (var i = 0; i < res.length/4; i++) {
+                var palabra = res[i].word;
+                relacionadas[i] = palabra;
+            }
+        })
+        return relacionadas;
+    }
 
-    function crearTemplate() {
+    function crearTemplate($padre) {
         var $estructura = $('<div class="palabra"><div class="palabra__caracter-wrapper"><h2 class="palabra__caracter"></h2></div><div class="palabra__info"><ul class="list-inline palabra__info-list"><li class="palabra__info-item palabra__info-item--pronunciacion"><span class="palabra__info-item-label">Pronunciación: </span><span class="palabra__info-item-value palabra__info-item-value--pronunciacion"></span><i class="fa fa-play palabra__info-item-play" aria-hidden="true"></i></li><li class="palabra__info-item palabra__info-item--traduccion"><span class="palabra__info-item-label">Significado: </span><span class="palabra__info-item-value palabra__info-item-value--significado"></span></li></ul></div></div>');
-        $resultados.append($estructura);
+        $padre.append($estructura);
         return $estructura;
     }
 
@@ -49,19 +79,18 @@ $(document).ready(function () {
         });
     }
 
-    function definirPalabra(traduccion, $elemento) {
-        $.post("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170321T235648Z.3193900a2fbf206f.5f78e83dd4de5da554f974b1976faff175639c28&text=" + traduccion + "&lang=es-en", function (res) {
-            var traduccionIngles = res.text[0].toLowerCase();
-            $.get("https://owlbot.info/api/v1/dictionary/" + traduccionIngles, function (res) {
-                var definicion = res[0].defenition;
-                $.post("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170321T235648Z.3193900a2fbf206f.5f78e83dd4de5da554f974b1976faff175639c28&text=" + definicion + "&lang=en-es", function (res) {
-                    var definincionEspanol = res.text[0];
-                    if (definincionEspanol.length < 50) {
-                        $elemento.text(definincionEspanol);
-                    } else {
-                        $elemento.text(definincionEspanol.substring(0, 50) + "...");
-                    }
-                });
+    function definirPalabra(traduccionIngles, $elemento) {
+        // Definición de la palabra buscada
+        $.get("https://owlbot.info/api/v1/dictionary/" + traduccionIngles, function (res) {
+            var definicion = res[0].defenition;
+            $.post("https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170321T235648Z.3193900a2fbf206f.5f78e83dd4de5da554f974b1976faff175639c28&text=" + definicion + "&lang=en-es", function (res) {
+                var definincionEspanol = res.text[0];
+                console.log(definincionEspanol);
+                if (definincionEspanol.length < 50) {
+                    $elemento.text(definincionEspanol);
+                } else {
+                    $elemento.text(definincionEspanol.substring(0, 50) + "...");
+                }
             });
         });
     }
